fb11b17c97739e9a75cccbde216316ffea2b6217
Jordi Condominas <jcondominas@gmail.com>
2018-05-13 17:25:11 +0200

    Rename file

--- src/todo/redux/reducers/reducers.js
+++ src/todo/redux/reducers/reducers.js
@@ -2,10 +2,10 @@ import {combineReducers} from 'redux'
 import {completedTodosReducer} from "./completedTodosReducer";
 import {todosReducer} from "./todosReducer";
 
-const rootState = (state = {}, action) => {
+const listTodosReducer = (state = {}, action) => {
     let todos = todosReducer(state.todos, action);
     let completedTodos = completedTodosReducer(state.completedTodos, action, state.todos);
     return {...state, todos: todos, completedTodos: completedTodos}
 };
 
-export default combineReducers({listTodos: rootState})
+export default combineReducers({listTodos: listTodosReducer})
