7df14af170fb288ae80c47f5ad11d11e25394253
Jordi Condominas <jcondominas@gmail.com>
2018-05-13 16:18:34 +0200

    Fix some warnings

--- src/todo/redux/reducers/index.js
+++ src/todo/redux/reducers/index.js
@@ -5,23 +5,20 @@ import {
 } from '../actions'
 
 const completedTodosReducer = (state = [], action, todos) => {
-    if (action.type === ADD_TODO) {
-        return state.concat(
+    if (action.type === REMOVE_TODO) {
+        return [...state,
             todos.find(todo => {
                 return todo.id === action.id
-            })
-        )
+            })];
     } else {
         return state
     }
-}
+};
 
 const todosReducer = (state = [], action) => {
     switch (action.type) {
         case ADD_TODO:
-            return state.concat(
-                {title: action.text, id: action.id}
-            );
+            return [...state, {title: action.text, id: action.id}];
         case REMOVE_TODO:
             return state.filter(todo => {
                     return todo.id !== action.id
@@ -36,6 +33,6 @@ const rootState = (state = {}, action) => {
     let todos = todosReducer(state.todos, action);
     let completedTodos = completedTodosReducer(state.completedTodos, action, state.todos);
     return {...state, todos: todos, completedTodos: completedTodos}
-}
+};
 
 export default combineReducers({listTodos: rootState})
